71e4fd6b732be6fcfa87cb3621a380b9b06e5c78
Adrplayer <adrian.becerra@utec.edu.pe>
2020-07-05 19:38:13 -0500

    Comentarios

--- ProyectoICC.py
+++ ProyectoICC.py
@@ -1,10 +1,23 @@
+#modulos
 import sqlite3
+
+#Conexión
 con=sqlite3.connect("mydatabase.db")
+
+# Obtención de datos
 n=input("Ingrese la ciudad de la locación del centro médico a agregar:")
 m=input("Ingrese el nombre del centro médico:")
 o=input("Ingrese la dirección exacta del centro médico:")
+
 print("Gracias por tu colaboración, así ayudarás a más peronas a encontrar un centro médico.")
+
 entities = (n, m , o)
+#   Te recomiendo que pongas como nombre en entities (Ciudad, Nombre, Dirección)
+#   para que no te estes seguro de datos son, pero no es necesario
+
+#Manejo - Base de datos:
 cursorObj=con.cursor()
 cursorObj.execute("INSERT INTO centrosmedicos(Ciudad, Nombre, Dirección) VALUES(?, ? ,?)", entities)
 con.commit()
+
+#Todo lo demás esta perfecto ;D 
